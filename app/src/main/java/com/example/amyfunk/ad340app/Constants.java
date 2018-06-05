d5f1141a3a409aac0bd60c56a9a60a7c80d648ed
Amy Funk <amy.y.funk@icloud.com>
2018-06-04 22:38:53 -0700

    Rolled back project

--- app/src/main/java/com/example/amyfunk/ad340app/Constants.java
+++ app/src/main/java/com/example/amyfunk/ad340app/Constants.java
@@ -1,10 +1,7 @@
 package com.example.amyfunk.ad340app;
 
-
-/**
- * Constant values reused in this sample.
- */
 public final class Constants {
+
     public static final int SUCCESS_RESULT = 0;
 
     public static final int FAILURE_RESULT = 1;
@@ -17,4 +14,5 @@ public final class Constants {
     public static final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY";
 
     public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_DATA_EXTRA";
+
 }
